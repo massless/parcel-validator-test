710ad6fbfeddbc49c955b658e988f4d899733ab1
Chris Wetherell <chrisw@gmail.com>
2020-05-13 20:27:58 -0700

    A workaround for validation that uses @typescript-eslint

--- app.tsx
+++ app.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import { render } from 'react-dom'
 
-const App = () => {
+interface Props {
+  foo: string;
+}
+
+const App = ({ foo }: Props): ReactElement => {
   // Should fail. Typescript validation.
-  const hello: string = 1 // eslint-disable-line no-unused-vars
+  const hello: number = { a: 1 }
+  console.log(hello)
 
   // Should NOT fail. The "as" operator is legal.
-  const world = 'world' as any // eslint-disable-line no-unused-vars
+  const world = 'world' as string
+  console.log(world)
 
   // Should fail. The eslint:recommended rules prohibit this.
   if (true) {}
 
-  return <h1>Ok</h1>
+  return <h1>ok?</h1>
 }
 
+// Should fail. React validation.
 render(<App />, document.getElementById('react'))
